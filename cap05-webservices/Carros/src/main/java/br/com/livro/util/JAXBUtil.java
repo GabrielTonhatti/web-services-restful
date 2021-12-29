f6376586ca12a063dc90fffe0326028bc9713168
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2021-12-29 17:37:20 -0300

    Iniciando capítulo 7 - Maven

--- cap05-webservices/Carros/src/main/java/br/com/livro/util/JAXBUtil.java
+++ cap05-webservices/Carros/src/main/java/br/com/livro/util/JAXBUtil.java
@@ -6,9 +6,14 @@ import java.io.StringWriter;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.stream.XMLStreamWriter;
+
+import org.codehaus.jettison.mapped.MappedNamespaceConvention;
+import org.codehaus.jettison.mapped.MappedXMLStreamWriter;
 
 import br.com.livro.domain.Carro;
 import br.com.livro.domain.ListaCarros;
+import br.com.livro.domain.Response;
 
 public class JAXBUtil {
 
@@ -22,7 +27,7 @@ public class JAXBUtil {
 	static {
 		try {
 			// Informa ao JAXB que é para gerar XML destas classes.
-			context = JAXBContext.newInstance(ListaCarros.class, Carro.class);
+			context = JAXBContext.newInstance(Response.class, ListaCarros.class, Carro.class);
 
 		} catch (JAXBException e) {
 			throw new RuntimeException(e);
@@ -38,6 +43,23 @@ public class JAXBUtil {
 			String xml = writer.toString();
 
 			return xml;
+
+		} catch (JAXBException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static String toJSON(Object object) throws IOException {
+		try {
+			StringWriter writer = new StringWriter();
+			Marshaller m = context.createMarshaller();
+			MappedNamespaceConvention con = new MappedNamespaceConvention();
+			XMLStreamWriter xmlStreamWriter = new MappedXMLStreamWriter(con, writer);
+			m.marshal(object, xmlStreamWriter);
+			String json = writer.toString();
+
+			return json;
 
 		} catch (JAXBException e) {
 			e.printStackTrace();
